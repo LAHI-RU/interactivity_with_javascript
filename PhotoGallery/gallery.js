8500b3e9b0c9a81f8a6bd13c0d87da2c975205d1
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-06-16 07:39:58 +0530

    pause

---
+++ PhotoGallery/gallery.js
@@ -0,0 +1,7 @@
+function upDate(previewPic) {
+  const update = document.getElementById("image").innerHTML;
+  update.innerHTML = previewPic.alt;
+  update.style.background = "url(+ previ";
+}
+
+function unDo() {}
